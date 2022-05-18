15daec595b8ed9963242d94ae8e2ec7608584e3e
Grant Moore <chinchalinchin@gmail.com>
2022-05-18 17:34:49 -0400

    add horizontal alt orientation and configure components to dynamically resize

--- web/app/src/app/app.config.ts
+++ web/app/src/app/app.config.ts
@@ -10,7 +10,7 @@ export enum orientations{
 export const APP_CONFIG: any = {
   title: 'Company Name',
   description: 'Company Description',
-  orientation: orientations.vertical_alt
+  orientation: orientations.horizontal_alt
 };
 
 export const ASSET_CONFIG: ImgConfig[] =[
